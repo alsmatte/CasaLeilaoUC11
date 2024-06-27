312c687699b7f5f2a04b0a09f3919564dd574f9d
Alexandra Matte <alsmatte.art@gmail.com>
2024-06-27 17:39:36 -0300

    Tela cadastroVIEW definida como tela principal

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,25 +4,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
+    
+    private String url = "jdbc:mysql://localhost/uc11", user = "root", password = "Cogumelos213";
     
     public Connection connectDB(){
         Connection conn = null;
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection(url,user,password);
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
